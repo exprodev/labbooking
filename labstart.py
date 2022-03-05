fe12dfbe8254d396b9e16d95fae7e9fce9bf79e8
exprosoftcorporation <k191115@nu.edu.pk>
2022-03-06 00:39:22 +0500

    Night commit

--- labstart.py
+++ labstart.py
@@ -1,147 +1,75 @@
 from datetime import date, timedelta
-
 from datetime import date
-
 import calendar
-
 import os
 
-
-
 daysLetter = ""
-
 fileString = ""
 
-
-
 #Date should be within a week (from Mon to Sun) so the string for the time_conf.txt can be produced correctly
-
 start_date = date(2022, 1, 25) 
-
 end_date = date(2022, 2, 1)
-
 teamID = ("lab1")
 
-
-
 print("Useful Info: ")
-
 print("SDate: ", start_date, "EDate : ", end_date, "\n")
 
-
-
 # returns timedelta
-
 delta = end_date - start_date
 
-
-
 for i in range(delta.days + 1):
-
     date = start_date + timedelta(days=i)
-
     day = calendar.day_name[date.weekday()]
-
     print(date, end=" ")
-
     print(day)
-
     daysLetter = daysLetter + day[:2]
-
-
 
 print (daysLetter)
 
-
-
 #Creating string for time_conf.txt
-
 daysLetter = daysLetter.replace(' ', '')
-
 timeLimit = "0000-2400"
-
-
-
 fileString = "sshd;*;" + teamID + ";" + daysLetter + timeLimit
-
-
-
 print("\nString to be adding in the file: ", fileString)
 
-
-
 #Open time_conf.txt#
-
 file1 = open("time_conf.txt","w")
-
 file1.write(fileString)
-
 file1.close()
-
-  
-
 file1 = open("time_conf.txt","r")
-
 print("\nOutput of Readlines after writing") 
-
 print(file1.readlines())
-
 print()
-
 file1.close()
-
-
 
 #Servers Health Log Initials
-
 server_id = 100
-
 initial_health = "Good (98%)"
 
-
-
 #Termination Script Path [!! Filename here has to be changed to 'terminate.sh' from 'script.sh', once the code is ready]
-
 script_path = "/var/terminate.sh"
 
-
-
 #Termination Date
-
 Termination_date = end_date + timedelta(days=1)
-
-
 
 #Tremination Script
 #Assumed that teamID is same as the userName
 terminate = "pkill -9 -u `id -u "+ teamID + "`;crontab -u root -l | grep -v '/bin/sh " + script_path + "'  | crontab -u root -; truncate -s 0 terminate.sh"
 
 #Open terminate.sh
-
 file1 = open("terminate.sh","w")
-
 file1.write(terminate)
-
 file1.close()
 
-
-
 #Crontab Addition
-
 cron = "(crontab -l ; echo " + '"' + "0 0 " + str(Termination_date.day) + " " + str(Termination_date.month) + " " + str(Termination_date.weekday()+1) + " /bin/sh " + script_path + '"' + ") | sort - | uniq - | crontab -"
-
 os.popen(cron)
 
 
 
 #Server Health Log
-
 final_health = "Fair (76%)"
-
 report = "Team ID: " + teamID + " || Start Date: " + str(start_date) + " || End Date: " + str(end_date) + " || Server ID: " + str(server_id) + " || Initial Health: " + initial_health + " || Final Health: " + final_health + "\n"
-
 file1 = open("server_health.txt","a")
-
 file1.write(report)
-
 file1.close()
--- script.js
+++ script.js
@@ -50,26 +50,55 @@ const renderCalendar = () => {
   let days = "";
 
   for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    let divClass = "";
+    let month = 0;
+    let year = 0;
+    if(date.getMonth()==0)
+    {
+      month = 11;
+      year = date.getFullYear()-1;
+    }
+    else
+    {
+      month = date.getMonth()-1;
+      year = date.getFullYear();
+    }
+    divClass = BookingCheck(new Date(year,month,prevLastDay - x + 1));
+    days += `<div class="prev-date ${divClass}">${prevLastDay - x + 1}</div>`;
   }
 
   for (let i = 1; i <= lastDay; i++) {
+    let divClass = "";
+    divClass = BookingCheck(new Date(date.getFullYear(),date.getMonth(),i));
+    let message = "";
+    message = BookedBy(divClass);
     if (
       i === new Date().getDate() &&
       date.getMonth() === new Date().getMonth() &&
       date.getFullYear() === new Date().getFullYear()
     ) {
-      // days += `<div class="today">${i}</div>`;
-      days += `<div class="today" onmouseover="InfoShow(${i})" onmouseout="InfoHide(${i})">${i}<span class="hide" id="${i}">Today!</span></div>`;
+      days += `<div class="today ${divClass}" onmouseover="InfoShow(${i})" onmouseout="InfoHide(${i})">${i}<span class="hide" id="${i}">${message}</span></div>`;
     } else {
-      // days += `<div>${i}</div>`;
-      days += `<div onmouseover="InfoShow(${i})" onmouseout="InfoHide(${i})">${i}<span class="hide" id="${i}">Other Day!</span></div>`;
+      days += `<div class="${divClass}" onmouseover="InfoShow(${i})" onmouseout="InfoHide(${i})">${i}<span class="hide" id="${i}">${message}</span></div>`;
     }
   }
 
   for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
-    
+    let divClass = "";
+    let month = 0;
+    let year = 0;
+    if(date.getMonth()==11)
+    {
+      month = 0;
+      year = date.getFullYear()+1;
+    }
+    else
+    {
+      month = date.getMonth()+1;
+      year = date.getFullYear();
+    }
+    divClass = BookingCheck(new Date(year,month,j));
+    days += `<div class="next-date ${divClass}">${j}</div>`;
   }
   monthDays.innerHTML = days;
 };
@@ -92,4 +121,25 @@ function InfoShow(ID) {
 
 function InfoHide(ID) {
   document.getElementById(ID).className = "hide";
+}
+
+function BookingCheck(theDate)
+{
+  console.log(theDate);
+  let StartDate = new Date(2022,1,28);
+  let EndDate = new Date(2022,2,8);
+  if(theDate >= StartDate && theDate <= EndDate)
+  {
+    return "BookedByMe";
+  }
+  return "myClass";
+}
+
+function BookedBy(divClass)
+{
+  if(divClass == "BookedByMe")
+  {
+    return "Reserved By Me";
+  }
+  else return "Available";
 }
